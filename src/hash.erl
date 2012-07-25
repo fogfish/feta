054c81248d47e4815f37dfceb82f5aeae44fe812
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2012-07-25 21:45:03 +0300

    minor cleanup of hash functions

--- src/hash.erl
+++ src/hash.erl
@@ -17,9 +17,9 @@
 %%
 -module(hash).
 
--export([ fnv32/1,  fnv32/2]).
--export([fnv32a/1, fnv32a/2]).
--export([fnv32m/1, fnv32m/2]).
+-export([ fnv32/1]).
+-export([fnv32a/1]).
+-export([fnv32m/1]).
 
 %%
 %%  FNV32 initial state
@@ -31,69 +31,52 @@
 %%
 %% fnv32(Data) -> Hash
 %%
-fnv32(Data) -> 
-	fnv32(Data, ?FNV32_INIT).
+fnv32(Data)
+ when is_list(Data) ->
+	fnv32(list_to_binary(Data), ?FNV32_INIT);
 
-fnv32([H|T], State) -> 
-	Hash  = ( ( State * ?FNV32_PRIME ) band ?FNV32_MASK ) bxor H,
-	fnv32(T, Hash);
-
-fnv32([], State) -> 
-	State;
+fnv32(Data)
+ when is_binary(Data) ->
+   fnv32(Data, ?FNV32_INIT).
 
 fnv32(<<H:8, T/bytes>>, State) -> 
 	Hash  = ( ( State * ?FNV32_PRIME ) band ?FNV32_MASK ) bxor H,
 	fnv32(T, Hash);
 
 fnv32(<<>>, State) -> 
-	State;
-
-fnv32(Val, State) when is_integer(Val)-> 
-	( ( State * ?FNV32_PRIME ) band ?FNV32_MASK ) bxor Val.
+	State.
 
 %%
 %%  fnv32a(Data) -> Hash
 %%
-fnv32a(Data) ->
-	fnv32a(Data, ?FNV32_INIT).
+fnv32a(Data)
+ when is_list(Data) ->
+	fnv32a(list_to_binary(Data), ?FNV32_INIT);
 
-fnv32a([H|T], State) -> 
-	Hash  =  ( ( State bxor H ) * ?FNV32_PRIME ) band ?FNV32_MASK,
-	fnv32a(T, Hash);
-
-fnv32a([], State) -> 
-	State;
+fnv32a(Data)
+ when is_binary(Data) ->
+   fnv32a(Data, ?FNV32_INIT).
 
 fnv32a(<<H:8, T/bytes>>, State) -> 
 	Hash  =  ( ( State bxor H ) * ?FNV32_PRIME ) band ?FNV32_MASK,
 	fnv32a(T, Hash);
 
 fnv32a(<<>>, State) -> 
-	State;
+	State.
 
-fnv32a(Val, State) when is_integer(Val)-> 
-	( ( State bxor Val ) * ?FNV32_PRIME ) band ?FNV32_MASK.
 
 %%
 %% fnv32m(Data) -> Hash
 %% 
 %% @see http://home.comcast.net/~bretm/hash/6.html
 %%
-fnv32m(Data) ->
-	fnv32m(Data, ?FNV32_INIT).
+fnv32m(Data)
+ when is_list(Data) ->
+	fnv32m(list_to_binary(Data), ?FNV32_INIT);
 
-fnv32m([H|T], State) -> 
-	Hash  =  ( ( State bxor H ) * ?FNV32_PRIME ) band ?FNV32_MASK,
-	fnv32m(T, Hash);
-
-fnv32m([], State) -> 
-	Hash1 = (State + (State bsl 13)) band ?FNV32_MASK,
-	Hash2 = (Hash1 bxor (Hash1 bsr 7)) band ?FNV32_MASK,
-	Hash3 = (Hash2 + (Hash2 bsl 3)) band ?FNV32_MASK,
-	Hash4 = (Hash3 bxor (Hash3 bsr 17)) band ?FNV32_MASK,
-	Hash5 = (Hash4 + (Hash4 bsl 5)) band ?FNV32_MASK,
-	Hash5;
-
+fnv32m(Data)
+ when is_binary(Data) ->
+   fnv32m(Data, ?FNV32_INIT).
 
 fnv32m(<<H:8, T/bytes>>, State) -> 
 	Hash  =  ( ( State bxor H ) * ?FNV32_PRIME ) band ?FNV32_MASK,
@@ -101,15 +84,6 @@ fnv32m(<<H:8, T/bytes>>, State) ->
 
 fnv32m(<<>>, State) -> 
 	Hash1 = (State + (State bsl 13)) band ?FNV32_MASK,
-	Hash2 = (Hash1 bxor (Hash1 bsr 7)) band ?FNV32_MASK,
-	Hash3 = (Hash2 + (Hash2 bsl 3)) band ?FNV32_MASK,
-	Hash4 = (Hash3 bxor (Hash3 bsr 17)) band ?FNV32_MASK,
-	Hash5 = (Hash4 + (Hash4 bsl 5)) band ?FNV32_MASK,
-	Hash5;
-
-fnv32m(Val, State) when is_integer(Val)-> 
-	Hash  =  ( ( State bxor Val ) * ?FNV32_PRIME ) band ?FNV32_MASK,
-	Hash1 = (Hash + (Hash bsl 13)) band ?FNV32_MASK,
 	Hash2 = (Hash1 bxor (Hash1 bsr 7)) band ?FNV32_MASK,
 	Hash3 = (Hash2 + (Hash2 bsl 3)) band ?FNV32_MASK,
 	Hash4 = (Hash3 bxor (Hash3 bsr 17)) band ?FNV32_MASK,
