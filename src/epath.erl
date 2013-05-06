40d52f7f9a39e37e6afb5048aadef9723c99ca12
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2013-05-06 16:02:23 +0300

    simplified epath is reverted

---
+++ src/epath.erl
@@ -0,0 +1,28 @@
+%% @description
+%%   XPath-like query for Erlang terms
+-module(epath).
+
+-export([
+   q/2
+]).
+
+q([Key | Tail], Term) ->
+   q(Tail, q_term(Key, Term));
+q([], Term) ->
+   Term.
+
+
+q_term(_, undefined) ->
+   undefined;
+q_term(Key, X)
+ when is_tuple(X) ->
+   erlang:element(Key, X);
+q_term(Key, X)
+ when is_integer(Key), is_list(X) ->
+   lists:nth(Key, X);
+q_term(Key, X)
+ when is_list(X) ->
+   case lists:keyfind(Key, 1, X) of
+      false    -> undefined;
+      {_, Val} -> Val
+   end.
