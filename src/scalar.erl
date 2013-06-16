1f41b21d283b5eb763b4a89247819bdd92a846c0
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2013-06-16 17:49:21 +0300

    scalar:decode; tempus:sec various formats

--- src/scalar.erl
+++ src/scalar.erl
@@ -23,11 +23,50 @@
 -module(scalar).
 
 -export([
-   s/1
+   decode/1, s/1
 ]).
 
 %%
+%% decode scalar type
+-spec(decode/1 :: (list() | binary()) -> any()).
+
+decode(<<"true">>)  ->
+   true;
+decode(<<"false">>) ->
+   false;
+decode(X)
+ when is_binary(X) ->
+   case re:run(X, "^(-?[0-9]+)(\\.[0-9]+)?([eE][+-]?[0-9])?$") of
+      {match, [_, _]}       -> btoi(X);
+      {match, [_, _, _]}    -> btof(X); 
+      {match, [_, _, _, _]} -> btof(X);
+      nomatch -> 
+         % binary is reference to part of large binary received from server
+         % copy binary to reduce size 
+         case binary:referenced_byte_size(X) of
+            Size when Size > 2 * byte_size(X) -> 
+               binary:copy(X);
+            _ -> 
+               X
+         end
+   end;
+decode("true") -> 
+   true;
+decode("false") -> 
+   false;
+decode(X)
+ when is_list(X) ->
+   case re:run(X, "^(-?[0-9]+)(\\.[0-9]+)?([eE][+-]?[0-9])?$") of
+      {match, [_, _]}       -> ltoi(X);
+      {match, [_, _, _]}    -> ltof(X); 
+      {match, [_, _, _, _]} -> ltof(X);
+      nomatch -> X
+   end.
+
+
+%%
 %% scalar to string
+-spec(s/1 :: (any()) -> list()).
 s(X) when is_atom(X)    -> atos(X);
 s(X) when is_list(X)    -> ltos(X);
 s(X) when is_integer(X) -> itos(X);
@@ -43,3 +82,17 @@ ftos(X) -> ltos(io_lib:format("~.9f", [X])).
 %%
 %% to list
 itol(X) -> integer_to_list(X).
+btol(X) -> binary_to_list(X).
+
+%%
+%% from list
+ltoi(X) -> list_to_integer(X).
+ltof(X) -> list_to_float(X).
+
+%%
+%% from binary
+btoi(X) -> ltoi(btol(X)).
+btof(X) -> ltof(btol(X)).
+
+
+
--- src/tempus.erl
+++ src/tempus.erl
@@ -18,12 +18,23 @@
 -module(tempus).
 
 -export([
-   sec/0,   sec/1, now/0, 
-   milli/0, milli/1, 
-   micro/0, micro/1,
+   % time convert utility
+   now/0, sec/0, sec/1, milli/0, milli/1, micro/0, micro/1,
 
-   shift/1, shift/2 
+   % time transform utility
+   inc/1, inc/2, dec/1, dec/2 
 ]).
+
+%% number of sec to Unix Epoch
+-define(UNX_EPOCH, 62167219200).
+
+%%%------------------------------------------------------------------
+%%%
+%%% convert
+%%%
+%%%------------------------------------------------------------------
+
+
 
 %%
 %% current time in seconds
@@ -35,8 +46,56 @@ now() ->
 sec() -> 
    sec(erlang:now()).
 
+%%
+%% convert time to second
 sec({Msec, Sec, _Usec}) ->
-   Msec * 1000000 + Sec.
+   % erlang:now
+   Msec * 1000000 + Sec;
+sec({{_,_,_},{_,_,_}}=Date) ->
+   % erlang date/time
+   calendar:datetime_to_gregorian_seconds(Date) - ?UNX_EPOCH;
+sec(Sec)
+ when is_list(Sec), length(Sec) =:= 8 ->
+   % any 8-length time is considered to be YYYYMMDD format
+   % 1970 - 1974 dates has to be requested as iso8601 or use scalar utility
+   sec(parser:iso8601(Sec));
+sec(T)
+ when is_list(T) ->
+   case string:chr(T, $T) of
+      0 -> 
+         case scalar:decode(T) of
+            X when is_list(X) -> sec(list_to_existing_atom(X));
+            X -> sec(X)
+         end;
+      _ -> sec(parser:iso8601(T))
+   end;
+sec(T)
+ when is_binary(T) ->
+   sec(binary_to_list(T));
+sec(today) ->
+   86400;
+sec(day)   ->
+   86400;
+sec(week)  -> 
+   7  * 86400;
+sec(month) -> 
+   30 * 86400;
+sec(year)  -> 
+   365 * 86400;
+sec(hour)  ->
+    3600;
+sec(Sec)
+ when is_float(Sec) ->
+   sec(erlang:round(Sec));
+sec(Sec)
+ when is_integer(Sec), Sec < 9999999999 ->
+   Sec; 
+sec(Milli)
+ when is_integer(Milli) ->
+   % given time value exceeds max allowed Sec value
+   Milli div 1000.
+
+
 
 %%
 %% current time in milliseconds   
@@ -54,14 +113,36 @@ micro() ->
 micro({Msec, Sec, Usec}) ->
    (Msec * 1000000 + Sec) * 1000000 + Usec.
 
+%%%------------------------------------------------------------------
+%%%
+%%% transform
+%%%
+%%%------------------------------------------------------------------
+
 %%
-%% add seconds
-shift(T) ->
-   shift(erlang:now(), T).
+%% increase time by T seconds
+inc(T) ->
+   inc(erlang:now(), T).
 
-shift({Msec, Sec, Usec}, T)
+inc({Msec, Sec, Usec}, T)
  when is_integer(T) ->
-   A = (Sec + T) rem 1000000,
-   B = (Sec + T) div 1000000,
-   {Msec + B, A, Usec}.
+   case Sec + T of
+      X when X =< 1000000 ->
+         {Msec, X, Usec};
+      X ->
+         {Msec + (X div 1000000), X rem 1000000, Usec}
+   end.
 
+%%
+%% increase time by T seconds
+dec(T) ->
+   dec(erlang:now(), T).
+
+dec({Msec, Sec, Usec}, T)
+ when is_integer(T) ->
+   case Sec - T of
+      X when X >= 0 ->
+         {Msec, X, Usec};
+      X ->
+         {Msec - 1, 1000000 + X, Usec}
+   end.
