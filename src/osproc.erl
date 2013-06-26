7bb44bcafdfe53064a54c47a4b12afe37423c9c8
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2013-06-26 12:17:33 +0300

    osproc:start

--- src/osproc.erl
+++ src/osproc.erl
@@ -2,6 +2,7 @@
 -behaviour(gen_server).
 
 -export([
+   start/1, start/2, start/3,
    start_link/1, start_link/2, start_link/3,
    init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3
 ]).
@@ -13,12 +14,17 @@
 
 %%
 %%
-start_link(Cmd) ->
-   gen_server:start_link(?MODULE, [Cmd, []], []).
+start(Cmd) ->
+   start(Cmd, []).
+start(Cmd, Args) ->
+   gen_server:start(?MODULE, [Cmd, Args], []).
+start(Name, Cmd, Args) ->
+   gen_server:start({local, Name}, ?MODULE, [Cmd, Args], []).
 
+start_link(Cmd) ->
+   start_link(Cmd, []).
 start_link(Cmd, Args) ->
    gen_server:start_link(?MODULE, [Cmd, Args], []).
-
 start_link(Name, Cmd, Args) ->
    gen_server:start_link({local, Name}, ?MODULE, [Cmd, Args], []).
 
@@ -29,7 +35,7 @@ init([Cmd, Args]) ->
       {spawn_executable, os:find_executable(sh)},
       [
          {args, ["-c", build_cmd(Cmd, Args)]},
-         exit_status
+         exit_status, use_stdio, stderr_to_stdout
       ]
    ),
    {ok, {Port, undefined}}.
