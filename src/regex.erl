2241afd4528fabd84a54e9ba6a2b68043e437251
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2013-08-16 23:23:30 +0300

    regex helper

---
+++ src/regex.erl
@@ -0,0 +1,82 @@
+%%
+%%   Copyright 2012 - 2013 Dmitry Kolesnikov, All Rights Reserved
+%%
+%%   Licensed under the Apache License, Version 2.0 (the "License");
+%%   you may not use this file except in compliance with the License.
+%%   You may obtain a copy of the License at
+%%
+%%       http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%   Unless required by applicable law or agreed to in writing, software
+%%   distributed under the License is distributed on an "AS IS" BASIS,
+%%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%   See the License for the specific language governing permissions and
+%%   limitations under the License.
+%%
+%% @description
+%%   regex helper
+-module(regex).
+
+-export([
+	m/2,
+	m/1,
+	p/2,
+	run/2
+]).
+
+-type(regex() :: re:mp() | binary() | list()).
+
+%%
+%% match string to regex
+-spec(m/2 :: (binary(), regex()) -> nomatch | [binary()]).
+
+m(String, RegEx)
+ when is_binary(String) ->
+ 	case re:run(String, RegEx) of
+ 		nomatch    -> 
+ 			nomatch;
+ 		{match, X} ->
+ 			[binary:part(String, At) || At <- X]
+ 	end.
+
+%%
+%% build regex match functor
+-spec(m/1 :: (regex()) -> function()).
+
+m(RegEx) ->
+	{ok, Ex} = re:compile(RegEx),
+ 	fun(String) -> regex:m(String, Ex) end.
+
+%%
+%% build pattern match functor, 
+%% function is executed if input string matched pattern 
+%% (bind function to regex)
+-spec(p/2 :: (regex(), function()) -> function()).
+
+p(RegEx, Fun) ->
+	{ok, Ex} = re:compile(RegEx),
+	fun(String) ->
+		case regex:m(String, Ex) of
+			nomatch ->
+				nomatch;
+			Match   ->
+				Fun(Match)
+		end
+	end.
+	
+%%
+%% run regex application
+-spec(run/2 :: (binary(), [function()]) -> nomatch | any()).
+
+run(String, [H | T])
+ when is_binary(String) ->
+ 	case H(String) of
+ 		nomatch ->
+ 			regex:run(String, T);
+ 		Result  ->
+ 			Result
+ 	end;
+
+run(_String, []) ->
+ 	nomatch.
+
