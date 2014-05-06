734617b261079bedd780d34e0291c4eedfcfb3d4
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2014-05-06 21:37:31 +0300

    remove legacy format / parser, use scalar and tempus instead

--- src/tempus.erl
+++ src/tempus.erl
@@ -48,6 +48,8 @@
    % time convert utility
    decode/1
   ,decode/2
+  ,encode/1
+  ,encode/2
 ]).
 -export_type([t/0, timer/0]).
 
@@ -452,7 +454,16 @@ day_of_week("Wed" ++ Tail) -> {3, Tail};
 day_of_week("Thu" ++ Tail) -> {4, Tail};
 day_of_week("Fri" ++ Tail) -> {5, Tail};
 day_of_week("Sat" ++ Tail) -> {6, Tail};
-day_of_week("Sun" ++ Tail) -> {7, Tail}.
+day_of_week("Sun" ++ Tail) -> {7, Tail};
+
+day_of_week(1) -> "Mon";
+day_of_week(2) -> "Tue";
+day_of_week(3) -> "Wed";
+day_of_week(4) -> "Thu";
+day_of_week(5) -> "Fri";
+day_of_week(6) -> "Sat";
+day_of_week(7) -> "Sun".
+
 
 full_day_of_week("Monday" ++ Tail)    -> {1, Tail};
 full_day_of_week("Tuesday" ++ Tail)   -> {2, Tail};
@@ -473,7 +484,20 @@ month_of_year("Aug" ++ Tail) -> {8, Tail};
 month_of_year("Sep" ++ Tail) -> {9, Tail};
 month_of_year("Oct" ++ Tail) -> {10, Tail};
 month_of_year("Nov" ++ Tail) -> {11, Tail};
-month_of_year("Dec" ++ Tail) -> {12, Tail}.
+month_of_year("Dec" ++ Tail) -> {12, Tail};
+month_of_year(1) -> "Jan";
+month_of_year(2) -> "Feb";
+month_of_year(3) -> "Mar";
+month_of_year(4) -> "Apr";
+month_of_year(5) -> "May";
+month_of_year(6) -> "Jun";
+month_of_year(7) -> "Jul";
+month_of_year(8) -> "Aug";
+month_of_year(9) -> "Sep";
+month_of_year(10) -> "Oct";
+month_of_year(11) -> "Nov";
+month_of_year(12) -> "Dec".
+
 
 full_month_of_year("January"  ++ Tail) -> {1, Tail};
 full_month_of_year("February" ++ Tail) -> {2, Tail};
@@ -489,6 +513,186 @@ full_month_of_year("November" ++ Tail) -> {11, Tail};
 full_month_of_year("December" ++ Tail) -> {12, Tail}.
 
 
+%%
+%% encode date time
+-spec(encode/1 :: (tempus:t()) -> list()).
+-spec(encode/2 :: (list(), tempus:t()) -> list()).
+
+encode(Val) ->
+   encode(?ISO8601, Val).
+
+encode(Fmt, Time)
+ when is_binary(Fmt) ->
+   encode(binary_to_list(Fmt), Time);
+   
+encode(Fmt, {{_Y,_M,_D}, {_H,_N,_S}}=Val) ->
+   encode(Fmt, Val, []);
+
+encode(Fmt, {_Mega, _Sec, _Micro}=Val) ->
+   encode(Fmt, calendar:now_to_universal_time(Val), []);
+
+encode(Fmt, Val) 
+ when is_integer(Val) ->
+   encode(Fmt, calendar:gregorian_seconds_to_datetime(Val + ?UNX_EPOCH), []).
+
+
+%% Day 
+%% %a  An abbreviated textual representation of the day Sun through Sat
+%% %A  A full textual representation of the day  Sunday through Saturday
+%% %d  Two-digit day of the month (with leading zeros) 01 to 31
+%% %e  Day of the month, with a space preceding single digits. 1 to 31
+%% %j  Day of the year, 3 digits with leading zeros  001 to 366
+%% %u  ISO-8601 numeric representation of the day of the week  1 (for Monday) though 7 (for Sunday)
+%% %w  Numeric representation of the day of the week 0 (for Sunday) through 6 (for Saturday)
+encode([$%, $a | Tail], {{_Y,_M,_D}=Date, {_H,_N,_S}}=Val, Acc) ->
+   Day = calendar:day_of_the_week(Date),
+   encode(Tail, Val, [day_of_week(Day) | Acc]);  
+
+encode([$%, $A | Tail], {{_Y,_M,_D}=Date, {_H,_N,_S}}=Val, Acc) ->
+   Day = calendar:day_of_the_week(Date),
+   encode(Tail, Val, [fday_of_week(Day) | Acc]);  
+
+encode([$%, $d | Tail], {{_Y,_M,D}, {_H,_N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [io_lib:format("~2.10.0B", [D]) | Acc]);
+
+encode([$%, $e | Tail], {{_Y,_M,D}, {_H,_N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [io_lib:format("~2.10. B", [D]) | Acc]);
+
+encode([$%, $j | Tail], {{Y,_M,_D}=Date, {_H,_N,_S}}=Val, Acc) ->
+   Day = calendar:date_to_gregorian_days(Date) - calendar:date_to_gregorian_days(Y-1, 12, 31),
+   encode(Tail, Val, [io_lib:format("~3.10.0B", [Day]) | Acc]);
+
+encode([$%, $u | Tail], {{_Y,_M,_D}=Date, {_H,_N,_S}}=Val, Acc) ->
+   Day = calendar:day_of_the_week(Date),
+   encode(Tail, Val, [io_lib:format("~B", [Day]) | Acc]);
+
+encode([$%, $w | Tail], {{_Y,_M,_D}=Date, {_H,_N,_S}}=Val, Acc) ->
+   Day = case calendar:day_of_the_week(Date) of
+      7 -> 0;
+      D -> D
+   end,
+   encode(Tail, Val, [io_lib:format("~B", [Day]) | Acc]);
+
+%%
+%% Week
+%% %U  Week number of the given year, starting with the first Sunday as the first week 13 (for the 13th full week of the year)
+%% %V  ISO-8601:1988 week number of the given year, starting with the first week of the year with at least 4 weekdays, with Monday being the start of the week 01 through 53 (where 53 accounts for an overlapping week)
+%% %W  A numeric representation of the week of the year, starting with the first Monday as the first week  46 (for the 46th week of the year beginning with a Monday)
+%% Not supported
+
+%%
+%% Month
+%% %b  Abbreviated month name, based on the locale Jan through Dec
+%% %B  Full month name, based on the locale  January through December
+%% %h  Abbreviated month name, based on the locale (an alias of %b)  Jan through Dec
+%% %m  Two digit representation of the month 01 (for January) through 12 (for December)
+encode([$%, $b | Tail], {{_Y, M,_D}, {_H,_N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [month_of_year(M) | Acc]);
+
+encode([$%, $B | Tail], {{_Y, M,_D}, {_H,_N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [fmonth_of_year(M) | Acc]);
+
+encode([$%, $m | Tail], {{_Y,M,_D}, {_H,_N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [io_lib:format("~2.10.0B", [M]) | Acc]);
+
+%%
+%% Year
+%% %C  Two digit representation of the century (year divided by 100, truncated to an integer)  19 for the 20th Century
+%% %g  Two digit representation of the year going by ISO-8601:1988 standards (see %V)  Example: 09 for the week of January 6, 2009
+%% %G  The full four-digit version of %g Example: 2008 for the week of January 3, 2009
+%% %y  Two digit representation of the year  Example: 09 for 2009, 79 for 1979
+%% %Y  Four digit representation for the year  Example: 2038
+
+encode([$%, $y | Tail], {{Y, _M,_D}, {_H,_N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [io_lib:format("~2.10.0B", [Y rem 1000])  | Acc]);
+
+encode([$%, $Y | Tail], {{Y, _M,_D}, {_H,_N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [io_lib:format("~4.10.0B", [Y])  | Acc]);
+
+%% 
+%% Time
+%% %H  Two digit representation of the hour in 24-hour format  00 through 23
+%% %I  Two digit representation of the hour in 12-hour format  01 through 12
+%% %l (lower-case 'L') Hour in 12-hour format, with a space preceeding single digits  1 through 12
+%% %M  Two digit representation of the minute  00 through 59
+%% %p  UPPER-CASE 'AM' or 'PM' based on the given time Example: AM for 00:31, PM for 22:23
+%% %P  lower-case 'am' or 'pm' based on the given time Example: am for 00:31, pm for 22:23
+%% %r  Same as "%I:%M:%S %p" Example: 09:34:17 PM for 21:34:17
+%% %R  Same as "%H:%M" Example: 00:35 for 12:35 AM, 16:44 for 4:44 PM
+%% %S  Two digit representation of the second  00 through 59
+%% %T  Same as "%H:%M:%S"  Example: 21:34:17 for 09:34:17 PM
+%% %X  Preferred time representation based on locale, without the date Example: 03:59:16 or 15:59:16
+%% %z  Either the time zone offset from UTC or the abbreviation (depends on operating system)  Example: -0500 or EST for Eastern Time
+%% %Z  The time zone offset/abbreviation option NOT given by %z (depends on operating system)  Example: -0500 or EST for Eastern Time
+
+encode([$%, $H | Tail], {{_Y, _M,_D}, {H,_N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [io_lib:format("~2.10.0B", [H])  | Acc]);
+
+encode([$%, $M | Tail], {{_Y, _M,_D}, {_H,N,_S}}=Val, Acc) ->
+   encode(Tail, Val, [io_lib:format("~2.10.0B", [N])  | Acc]);
+
+encode([$%, $S | Tail], {{_Y, _M,_D}, {_H,_N,S}}=Val, Acc) ->
+   encode(Tail, Val, [io_lib:format("~2.10.0B", [S])  | Acc]);
+
+%% Time and Date Stamps
+%% %c  Preferred date and time stamp based on local  Example: Tue Feb 5 00:45:10 2009 for February 5, 2009 at 12:45:10 AM
+%% %D  Same as "%m/%d/%y"  Example: 02/05/09 for February 5, 2009
+%% %F  Same as "%Y-%m-%d" (commonly used in database datestamps) Example: 2009-02-05 for February 5, 2009
+%% %s  Unix Epoch Time timestamp (same as the time() function) Example: 305815200 for September 10, 1979 08:40:00 AM
+%% %x  Preferred date representation based on locale, without the time Example: 02/05/09 for February 5, 2009
+encode([$%, $s | Tail], {{_Y,_M,_D}, {_H,_N,_S}}=Val, Acc) ->
+   Sec = calendar:datetime_to_gregorian_seconds(Val) - ?UNX_EPOCH,
+   encode(Tail, Val, [io_lib:format("~B", [Sec]) | Acc]);
+
+%%
+%% Miscellaneous
+%% %n  A newline character ("\n")  ---
+%% %t  A Tab character ("\t")
+encode([H | Tail], Val, Acc) ->
+   encode(Tail, Val, [H | Acc]);
+
+encode([], _Val, Acc) ->
+   lists:flatten(lists:reverse(Acc)).
+
+
+%%-------------------------------------------------------------------
+%%
+%% private
+%%
+%%-------------------------------------------------------------------
+
+%%
+%% day of week
+
+%%
+%% full day of week
+fday_of_week(1) -> "Monday";
+fday_of_week(2) -> "Tuesday";
+fday_of_week(3) -> "Wednesday";
+fday_of_week(4) -> "Thursday";
+fday_of_week(5) -> "Friday";
+fday_of_week(6) -> "Saturday";
+fday_of_week(7) -> "Sunday".
+
+
+%%
+%% month of year
+
+
+%%
+%% full month of year
+fmonth_of_year(1) -> "January";
+fmonth_of_year(2) -> "February";
+fmonth_of_year(3) -> "March";
+fmonth_of_year(4) -> "April";
+fmonth_of_year(5) -> "May";
+fmonth_of_year(6) -> "June";
+fmonth_of_year(7) -> "July";
+fmonth_of_year(8) -> "August";
+fmonth_of_year(9) -> "September";
+fmonth_of_year(10) -> "October";
+fmonth_of_year(11) -> "November";
+fmonth_of_year(12) -> "December".
 
 
 
